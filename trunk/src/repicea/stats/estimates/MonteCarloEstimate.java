e3ada417db8b7a55c7860662fd5691e3c07f28f0
donenrique <donenrique@14d3ad2c-f47e-48ea-b096-69a619d0e6a4>
2016-01-11 15:19:50 +0000

    New static methods in MonteCarloEstimate class

--- trunk/src/repicea/stats/estimates/MonteCarloEstimate.java
+++ trunk/src/repicea/stats/estimates/MonteCarloEstimate.java
@@ -18,6 +18,7 @@
  */
 package repicea.stats.estimates;
 
+import java.security.InvalidParameterException;
 import java.util.List;
 
 import repicea.math.Matrix;
@@ -44,5 +45,39 @@ public class MonteCarloEstimate extends Estimate<NonparametricDistribution> {
 	public void addRealization(Matrix value) {getDistribution().addRealization(value);}
 	
 	public List<Matrix> getRealizations() {return getDistribution().getRealizations();}
+
+	/**
+	 * This method returns a MonteCarloEstimate instance that results from the subtraction of two 
+	 * MonteCarloEstimate instances with the same number of realizations. 
+	 * @param estimate1 the first estimate
+	 * @param estimate2 the estimate that is subtracted to the first estimate
+	 * @return a MonteCarloEstimate instance
+	 */
+	public static MonteCarloEstimate computeDifference(MonteCarloEstimate estimate1, MonteCarloEstimate estimate2) {
+		if (estimate1.getNumberOfRealizations() != estimate2.getNumberOfRealizations()) {
+			throw new InvalidParameterException("The number of realizations is not consistent!");
+		}
+		MonteCarloEstimate outputEstimate = new MonteCarloEstimate();
+		for (int i = 0; i < estimate1.getNumberOfRealizations(); i++) {
+			outputEstimate.addRealization(estimate1.getRealizations().get(i).subtract(estimate2.getRealizations().get(i)));
+		}
+		return outputEstimate;
+	}
+	
+	/**
+	 * This method returns a MonteCarloEstimate instance that results from the product of original 
+	 * MonteCarloEstimate instance and a scalar. 
+	 * @param estimate1 the first estimate
+	 * @param scalar the multiplication factor
+	 * @return a MonteCarloEstimate instance
+	 */
+	public static MonteCarloEstimate multiply(MonteCarloEstimate estimate1, double scalar) {
+		MonteCarloEstimate outputEstimate = new MonteCarloEstimate();
+		for (int i = 0; i < estimate1.getNumberOfRealizations(); i++) {
+			outputEstimate.addRealization(estimate1.getRealizations().get(i).scalarMultiply(scalar));
+		}
+		return outputEstimate;
+	}
+
 	
 }
