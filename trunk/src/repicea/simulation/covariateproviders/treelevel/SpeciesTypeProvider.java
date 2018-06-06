6bfe0cfc4dd7c4fd41506cd7fd981dae2c1a133c
donenrique <donenrique@14d3ad2c-f47e-48ea-b096-69a619d0e6a4>
2018-06-06 13:17:39 +0000

    New interface SpeciesTypeProvider

---
+++ trunk/src/repicea/simulation/covariateproviders/treelevel/SpeciesTypeProvider.java
@@ -0,0 +1,55 @@
+/*
+ * This file is part of the repicea-simulation library.
+ *
+ * Copyright (C) 2009-2018 Mathieu Fortin for Rouge-Epicea
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed with the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * Please see the license at http://www.gnu.org/copyleft/lesser.html.
+ */
+package repicea.simulation.covariateproviders.treelevel;
+
+import repicea.util.REpiceaTranslator;
+import repicea.util.REpiceaTranslator.TextableEnum;
+
+/**
+ * This interface ensures that the tree can return its species type, i.e. Coniferous or Broadleaved.
+ * @author Mathieu Fortin - June 2018
+ */
+public abstract interface SpeciesTypeProvider {
+
+	
+	
+	public static enum SpeciesType implements TextableEnum {
+		ConiferousSpecies("Coniferous","Conif\u00E8re"),
+		BroadleavedSpecies("Broadleaved", "Feuillue");
+
+		SpeciesType(String englishText, String frenchText) {
+			setText(englishText, frenchText);
+		}
+		
+		@Override
+		public void setText(String englishText, String frenchText) {
+			REpiceaTranslator.setString(this, englishText, frenchText);
+		}
+		
+		@Override
+		public String toString() {return REpiceaTranslator.getString(this);}
+	}
+
+	/**
+	 * This method returns the species type of the tree.
+	 * @return a SpeciesType enum
+	 */
+	public SpeciesType getSpeciesType();
+
+}
