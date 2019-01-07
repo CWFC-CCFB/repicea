3867584b3fab77dce4a2c7e97269adc32da41769
donenrique <50317186+donenrique24@users.noreply.github.com>
2019-01-07 14:19:25 +0000

    RGatewayServer development

--- test/repicea/net/server/ServerTests.java
+++ test/repicea/net/server/ServerTests.java
@@ -3,6 +3,7 @@ package repicea.net.server;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
+import java.util.Vector;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -39,7 +40,7 @@ public class ServerTests {
 		 
 		InetSocketAddress socketAddress = new InetSocketAddress(InetAddress.getLoopbackAddress(), 18000);
 		FakeClient client = new FakeClient(socketAddress, false);	// false: it does as if was not a Java application
-		Object arrayListRepresentation = client.createAnArrayList();
+		Object arrayListRepresentation = client.createAnArrayList(); // creates a FakeArrayList object which will be used to test the Double vs double method call
 		Assert.assertTrue(arrayListRepresentation != null);
 		Assert.assertTrue(arrayListRepresentation.toString().startsWith("JavaObject;repicea.net.server.FakeArrayList"));
 		int hashCode = Integer.parseInt(arrayListRepresentation.toString().substring(arrayListRepresentation.toString().indexOf("@") + 1));
@@ -58,7 +59,28 @@ public class ServerTests {
 		callback = client.testThisDoubleWrapper(arrayListRepresentation);		// here we test if a method with Double as argument will be match to a double 
 		Assert.assertTrue(callback != null);
 		Assert.assertTrue(callback.toString().equals("numeric0.0"));
-				
+
+		callback = client.createAVectorWithArguments();
+		Assert.assertTrue(callback != null);
+		hashCode = Integer.parseInt(callback.toString().substring(callback.toString().indexOf("@") + 1));
+		Vector listWithCapacityOf3 = (Vector) env.get(hashCode);
+		Assert.assertEquals("Testing capacity", listWithCapacityOf3.capacity(), 3);
+
+		callback = client.createMultipleVectorWithArguments();			// creating several objects at once
+		Assert.assertTrue(callback != null);
+		String[] objectReps = callback.toString().split(",");
+		int expCapacity = 3;
+		for (String objRep : objectReps) {
+			hashCode = Integer.parseInt(objRep.toString().substring(objRep.toString().indexOf("@") + 1));
+			Vector vec = (Vector) env.get(hashCode);
+			Assert.assertEquals("Testing capacity", vec.capacity(), expCapacity);
+			expCapacity++;
+		}
+//		hashCode = Integer.parseInt(callback.toString().substring(callback.toString().indexOf("@") + 1));
+//		Vector listWithCapacityOf3 = (Vector) env.get(hashCode);
+//		Assert.assertEquals("Testing capacity", listWithCapacityOf3.capacity(), 3);
+
+		int u = 0;
 		// TODO handle multiple  caller as in the vector logic 
 		client.close();			
 		System.out.println("Server implementation with multiple requests (3) successfully tested!");
