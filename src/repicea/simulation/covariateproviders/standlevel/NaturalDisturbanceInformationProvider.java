514d0f3a684597d638edc3098535b9dea8cd5edb
donenrique <50317186+donenrique24@users.noreply.github.com>
2019-03-29 08:45:31 +0000

    New interface for natural disturbance information

---
+++ src/repicea/simulation/covariateproviders/standlevel/NaturalDisturbanceInformationProvider.java
@@ -0,0 +1,41 @@
+/*
+ * This file is part of the repicea library.
+ *
+ * Copyright (C) 2009-2019 Mathieu Fortin for Rouge-Epicea
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed with the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * Please see the license at http://www.gnu.org/copyleft/lesser.html.
+ */
+package repicea.simulation.covariateproviders.standlevel;
+
+/**
+ * This interface ensures the instance can provide either the time since the last
+ * disturbance or at least the time since the first known date.
+ * @author Mathieu Fortin - March 2019
+ */
+public interface NaturalDisturbanceInformationProvider {
+
+	/**
+	 * This method returns the time since the last disturbance or null if this time is unknown.
+	 * @return an Integer instance or null
+	 */
+	public Integer getTimeSinceLastDisturbanceYrs();
+	
+	/**
+	 * This method returns the time since the first known date. This is a work-around if the 
+	 * getTimeSinceLastDisturbanceYrs method returns null.
+	 * @return a positive integer
+	 */
+	public int getTimeSinceFirstKnownDateYrs();
+	
+}
