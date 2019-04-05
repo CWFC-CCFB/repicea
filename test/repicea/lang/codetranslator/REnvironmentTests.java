0903fd4397899b6a201a7aa60bd3bf20f8697d90
donenrique <50317186+donenrique24@users.noreply.github.com>
2019-04-05 13:47:24 +0000

    Refactoring in REnvironment (better splitters)

--- test/repicea/lang/codetranslator/REnvironmentTests.java
+++ test/repicea/lang/codetranslator/REnvironmentTests.java
@@ -9,9 +9,12 @@ public class REnvironmentTests {
 	@Test
 	public void creatingAnArrayTest() throws Exception {
 		REnvironment r = new REnvironment();
-		System.out.println(int.class.getName());
-		Object callback = r.processCode("createarray;int;integer3;integer2");
-		Assert.assertTrue("Testing if the callback is formatted for an array", callback.toString().startsWith("JavaObject;[[I"));
+//		System.out.println(int.class.getName());
+		Object callback = r.processCode("createarray" + 
+				REnvironment.MainSplitter + "int" + 
+				REnvironment.MainSplitter + "integer3" + 
+				REnvironment.MainSplitter + "integer2");
+		Assert.assertTrue("Testing if the callback is formatted for an array", callback.toString().startsWith("JavaObject" + REnvironment.MainSplitter + "[[I"));
 	}
 	
 }
