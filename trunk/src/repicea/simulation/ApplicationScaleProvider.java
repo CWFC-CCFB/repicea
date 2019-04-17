097247df123a53861f2266fe3138fc776f04e746
donenrique <donenrique@14d3ad2c-f47e-48ea-b096-69a619d0e6a4>
2019-04-17 12:34:59 +0000

    New ManagementTypeProvider interface

--- trunk/src/repicea/simulation/ApplicationScaleProvider.java
+++ trunk/src/repicea/simulation/ApplicationScaleProvider.java
@@ -1,5 +1,5 @@
 /*
- * This file is part of the repicea-simulation library.
+ * This file is part of the repicea library.
  *
  * Copyright (C) 2009-2019 Mathieu Fortin for Rouge-Epicea
  *
@@ -16,7 +16,7 @@
  *
  * Please see the license at http://www.gnu.org/copyleft/lesser.html.
  */
-package repicea.simulation.covariateproviders.standlevel;
+package repicea.simulation;
 
 import repicea.util.REpiceaTranslator;
 import repicea.util.REpiceaTranslator.TextableEnum;
