8755e5d2a9cfae8c5c5182a00b7aaf62f12562fa
donenrique <donenrique@14d3ad2c-f47e-48ea-b096-69a619d0e6a4>
2019-08-15 17:16:55 +0000

    New classes for pattern detection in large datasets

--- trunk/src/repicea/stats/data/DataPattern.java
+++ trunk/src/repicea/stats/data/DataPattern.java
@@ -25,11 +25,35 @@ import java.util.List;
 import java.util.Map;
 
 import repicea.gui.genericwindows.REpiceaProgressBarDialog;
+import repicea.stats.data.DataSequence.ActionOnPattern;
+import repicea.stats.data.DataSequence.Mode;
 import repicea.stats.data.DataSetGroupMap.PatternMode;
 import repicea.util.ObjectUtility;
 
-public class DataPattern extends ArrayList<Object> {
+public class DataPattern extends ArrayList<Object> implements Cloneable {
 
+	protected enum Action {Replace,
+		Add;
+	}
+	
+	protected final int fieldIndex;
+	protected final DataPatternMap dataPatternMap;
+	
+	protected DataPattern(int fieldIndex, DataPatternMap dataPatternMap) {
+		this.fieldIndex = fieldIndex;
+		this.dataPatternMap = dataPatternMap;
+	}
+	
+	private DataPattern() {
+		fieldIndex = -1;
+		dataPatternMap = null;
+	}
+	
+	/**
+	 * Returns a DataPattern instance without exclusions.
+	 * @param exclusions
+	 * @return
+	 */
 	private DataPattern getCleanClone(List<Object> exclusions) {
 		DataPattern clone = new DataPattern();
 		for (Object obj : this) {
@@ -38,6 +62,130 @@ public class DataPattern extends ArrayList<Object> {
 			}
 		}
 		return clone;
+	}
+	
+	/**
+	 * Returns a DataPattern trimmed for exclusions
+	 * @param exclusions
+	 * @return
+	 */
+	protected DataPattern getTrimmedPattern(List<Object> exclusions) {
+		if (exclusions != null && !exclusions.isEmpty()) {
+			DataPattern cleanPattern = new DataPattern();
+			int i = 0;
+			while (i < size() && exclusions.contains(get(i))) {
+				i++;
+			}
+			int j = size() - 1;
+			while (j >= 0 && exclusions.contains(get(j))) {
+				j--;
+			}
+			for (int k = i; k <= j; k++) {
+				cleanPattern.add(get(k));
+			}
+			return cleanPattern;
+		} else {
+			return this;
+		}
+	}
+	
+
+
+//	protected boolean doesFitInThisSequence(DataSequence sequence, List<Object> exclusions, boolean isEmptyPatternAccepted) {
+//		DataPattern cleanPattern = getTrimmedPattern(exclusions);
+//		if (cleanPattern == null || cleanPattern.isEmpty()) {
+//			return isEmptyPatternAccepted;
+//		} else  if (cleanPattern.size() == 1) {
+//			Object singleObj = cleanPattern.get(0);
+//			return sequence.containsKey(singleObj);
+//		} else {
+//			for (int i = 1; i < cleanPattern.size(); i++) {
+//				Object obj0 = cleanPattern.get(i - 1);
+//				Object obj1 = cleanPattern.get(i);
+//				if (!sequence.containsKey(obj0) || !sequence.get(obj0).contains(obj1)) {
+//					return false;
+//				} 
+//			}
+//			return true;
+//		}
+//	}
+
+//	protected Integer doesPartlyFitInThisSequence(DataSequence sequence, List<Object> exclusions, boolean isEmptyPatternAccepted) {
+//		if (size() > 2) {
+//			for (int i = 1; i < size(); i++) {
+//				Object obj0 = get(i - 1);
+//				Object obj1 = get(i);
+//				if (sequence.containsKey(obj0) && sequence.get(obj0).contains(obj1)) {
+//					return i;
+//				} 
+//			}
+//		} 
+//		return null;
+//	}
+
+	/**
+	 * Returns the object that is prevalent in terms of frequency. A greater
+	 * weight is given to last measurement under the assumption that the ranking
+	 * has been done according to the date.
+	 * @param exclusions
+	 * @return
+	 */
+	protected Object getEmergingObject(List<Object> exclusions) {
+		DataPattern clone = getCleanClone(exclusions);
+		Map<Object, Double> rankingMap = new HashMap<Object, Double>();
+		for (int i = 0; i < clone.size(); i++) {
+			Object obj = clone.get(i);
+			double previousValue = 0d;
+			if (rankingMap.containsKey(obj)) {
+				previousValue = rankingMap.get(obj);
+			} 
+			rankingMap.put(obj, previousValue + i * .5 + 1);
+		}
+		double maxValue = 1d;
+		Object winningObject = null;
+		for (Object obj : rankingMap.keySet()) {
+			double rank = rankingMap.get(obj);
+			if (rank > maxValue + 1) {
+				maxValue = rank;
+				winningObject = obj;
+			}
+		}
+		return winningObject;
+	}
+	
+	protected DataPattern getSubDataPattern(int start, int end) {
+		DataPattern subPattern = new DataPattern();
+		for (Object obj : this) {
+			subPattern.add(obj.toString().substring(start, end));
+		}
+		return subPattern;
+	}
+	
+	/**
+	 * Uses a sub pattern typically the first two characters of a String to see
+	 * if there is a homogeneous pattern.
+	 * @param exclusions
+	 * @param start
+	 * @param end
+	 * @return
+	 */
+	protected Object getLastButSimilar(List<Object> exclusions, int start, int end) {
+		DataPattern clone = getCleanClone(exclusions);
+		DataPattern subPattern = clone.getSubDataPattern(start, end);
+		if (subPattern.getHomogeneousObject(null) != null) {
+			return clone.get(clone.size() - 1);
+		} else {
+			return null;
+		}
+	}
+	
+	protected Object getLastObject(List<Object> exclusions) {
+		DataPattern clone = getCleanClone(exclusions);
+		if (clone.size() > 0) {
+			return clone.get(clone.size() - 1);
+		} else {
+			return null;
+		}
 	}
 	
 	protected Object getHomogeneousObject(List<Object> exclusions) {
@@ -64,120 +212,66 @@ public class DataPattern extends ArrayList<Object> {
 		return field;
 	}
 
-
-		
-	protected Object getEmergingObject(List<Object> exclusions) {
-		DataPattern clone = getCleanClone(exclusions);
-		Map<Object, Double> rankingMap = new HashMap<Object, Double>();
-		for (int i = 0; i < clone.size(); i++) {
-			Object obj = clone.get(i);
-			double previousValue = 0d;
-			if (rankingMap.containsKey(obj)) {
-				previousValue = rankingMap.get(obj);
-			} 
-			rankingMap.put(obj, previousValue + i * .5 + 1);
+	@Override
+	public Object set(int i, Object newValue) {
+		List<DataGroup> groups = dataPatternMap.get(this);
+		for (DataGroup group : groups) {
+			DataSet ds = dataPatternMap.dataSetGroupMap.get(group);
+			ds.setValueAt(i, fieldIndex, newValue);
 		}
-		double maxValue = 1d;
-		Object winningObject = null;
-		for (Object obj : rankingMap.keySet()) {
-			double rank = rankingMap.get(obj);
-			if (rank > maxValue + 1) {
-				maxValue = rank;
-				winningObject = obj;
-			}
-		}
-		return winningObject;
-	}
-	
-	protected DataPattern getDataPattern(int start, int end) {
-		DataPattern subPattern = new DataPattern();
-		for (Object obj : this) {
-			subPattern.add(obj.toString().substring(start, end));
-		}
-		return subPattern;
-	}
-	
-	
-	protected Object getLastButSimilar(List<Object> exclusions, int start, int end) {
-		DataPattern clone = getCleanClone(exclusions);
-		DataPattern subPattern = clone.getDataPattern(start, end);
-		if (subPattern.getHomogeneousObject(null) != null) {
-			return clone.get(clone.size() - 1);
-		} else {
-			return null;
-		}
-	}
-	
-	protected Object getLastObject(List<Object> exclusions) {
-		DataPattern clone = getCleanClone(exclusions);
-		if (clone.size() > 0) {
-			return clone.get(clone.size() - 1);
-		} else {
-			return null;
-		}
-	}
-	
-	protected DataPattern getCleanPattern(List<Object> exclusions) {
-		if (exclusions != null && !exclusions.isEmpty()) {
-			DataPattern cleanPattern = new DataPattern();
-			int i = 0;
-			while (i < size() && exclusions.contains(get(i))) {
-				i++;
-			}
-			int j = size() - 1;
-			while (j >= 0 && exclusions.contains(get(j))) {
-				j--;
-			}
-			for (int k = i; k <= j; k++) {
-				cleanPattern.add(get(k));
-			}
-			return cleanPattern;
-		} else {
-			return this;
-		}
-	}
-	
-
-
-	protected boolean doesFitInThisSequence(DataSequence sequence, List<Object> exclusions, boolean isEmptyPatternAccepted) {
-		DataPattern cleanPattern = getCleanPattern(exclusions);
-		if (cleanPattern == null || cleanPattern.isEmpty()) {
-			return isEmptyPatternAccepted;
-		} else  if (cleanPattern.size() == 1) {
-			Object singleObj = cleanPattern.get(0);
-			return sequence.containsKey(singleObj);
-		} else {
-			for (int i = 1; i < cleanPattern.size(); i++) {
-				Object obj0 = cleanPattern.get(i - 1);
-				Object obj1 = cleanPattern.get(i);
-				if (!sequence.containsKey(obj0) || !sequence.get(obj0).contains(obj1)) {
-					return false;
-				} 
-			}
-			return true;
-		}
-	}
-
-	protected Integer doesPartlyFitInThisSequence(DataSequence sequence, List<Object> exclusions, boolean isEmptyPatternAccepted) {
-		if (size() > 2) {
-			for (int i = 1; i < size(); i++) {
-				Object obj0 = get(i - 1);
-				Object obj1 = get(i);
-				if (sequence.containsKey(obj0) && sequence.get(obj0).contains(obj1)) {
-					return i;
-				} 
-			}
-		} 
 		return null;
 	}
 
+	protected void comment(String str) {
+		for (int i = 0; i < size(); i++) {
+			comment(i, str);
+		}
+	}
+
+	protected void comment(int i, String str) {
+		DataSet originalDataSet = dataPatternMap.dataSetGroupMap.originalDataSet; 
+		if (!originalDataSet.getFieldNames().contains("JavaComments")) {
+			int size = originalDataSet.getNumberOfObservations();
+			Object[] values = new Object[size];
+			for (int j = 0; j < values.length; j++) {
+				values[j] = "";
+			}
+			originalDataSet.addField("JavaComments", values);
+		}
+		updateField(i, "JavaComments", str, Action.Add);
+	}
+	
+	protected void updateField(int i, String fieldName, Object newValue, Action action) {
+		List<DataGroup> groups = dataPatternMap.get(this);
+		for (DataGroup group : groups) {
+			DataSet ds = dataPatternMap.dataSetGroupMap.get(group);
+			Object formerValue = ds.getValueAt(i, fieldName);
+			if (action == Action.Replace) {
+				ds.setValueAt(i, fieldName, newValue);
+			} else if (action == Action.Add) {
+				Object addedNewValue;
+				if (formerValue instanceof Number && newValue instanceof Number) {
+					addedNewValue = ((Number) newValue).doubleValue() + ((Number) formerValue).doubleValue();
+				} else {
+					addedNewValue = formerValue.toString().concat(newValue.toString());
+				}
+				ds.setValueAt(i, fieldName, addedNewValue);
+			}
+		}
+	}
+	
+	
+	
+	@Override
+	public DataPattern clone() {
+		DataPattern clone = new DataPattern(fieldIndex, dataPatternMap);
+		clone.addAll(this);
+		return clone;
+	}
+	
 	
 	
 	public static void main(String[] args) throws IOException {
-
-
-		
-		
 		String filename = ObjectUtility.getPackagePath(DataSet.class) + "trees.csv";
 		DataSet dataSet = new DataSet(filename, false);
 		new REpiceaProgressBarDialog("Reading inventory", "...", dataSet, false);
@@ -225,7 +319,14 @@ public class DataPattern extends ArrayList<Object> {
 		renumberedStatuses.add(50.0);
 		renumberedStatuses.add(52.0);
 		
-		DataSequence acceptableDataSequence = new DataSequence();
+		ActionOnPattern action = new ActionOnPattern() {
+			@Override
+			protected void doAction(DataPattern pattern, Object...parms) {
+				pattern.comment("status = C");
+			}
+		};
+		
+		DataSequence acceptableDataSequence = new DataSequence(true, Mode.Total, action);
 
 		List<Object> alives = new ArrayList<Object>();
 		alives.addAll(aliveStatuses);
@@ -271,10 +372,27 @@ public class DataPattern extends ArrayList<Object> {
 		for (Object obj : terminalStatuses) {
 			possibleOutcomes = new ArrayList<Object>();
 			possibleOutcomes.addAll(terminalStatuses);
+			possibleOutcomes.addAll(deadStatuses);
 			acceptableDataSequence.put(obj, possibleOutcomes);
 		}
 		
-		DataSequence twoDifferentTreesSequence = new DataSequence();
+		
+		action = new ActionOnPattern() {
+			@Override
+			protected void doAction(DataPattern pattern, Object... parms) {
+				int observationIndex = (Integer) parms[0];
+				for (int i = 0; i < pattern.size(); i++) {
+					if (i >= observationIndex) {
+						pattern.updateField(i, "NO_ARBRE", 1000, Action.Add);
+						pattern.comment(i, "renumbered");
+					} else {
+						pattern.comment(i, "status = C");
+					}
+				}
+			}
+		};
+		
+		DataSequence twoDifferentTreesSequence = new DataSequence(false, Mode.Partial, action);
 		List<Object> deadOrMissingStatuses = new ArrayList<Object>();
 		deadOrMissingStatuses.addAll(terminalStatuses);
 		deadOrMissingStatuses.add("NA");
@@ -285,7 +403,18 @@ public class DataPattern extends ArrayList<Object> {
 			twoDifferentTreesSequence.put(obj, possibleOutcomes);
 		}
 		
-		
+
+		DataSequence measurementErrorSequence = new DataSequence(false, Mode.Partial, action);
+		List<Object> deadOrMissingStatuses = new ArrayList<Object>();
+		deadOrMissingStatuses.addAll(terminalStatuses);
+		deadOrMissingStatuses.add("NA");
+		for (Object obj : deadOrMissingStatuses) {
+			possibleOutcomes = new ArrayList<Object>();
+			possibleOutcomes.addAll(recruitStatuses);
+			possibleOutcomes.addAll(recruitDeadStatuses);
+			twoDifferentTreesSequence.put(obj, possibleOutcomes);
+		}
+
 		
 		dataSetGroupMap.patternize(PatternMode.Sequence, 3, exclusions, acceptableDataSequence, twoDifferentTreesSequence);
 
@@ -294,10 +423,10 @@ public class DataPattern extends ArrayList<Object> {
 
 		
 		
-		DataSet correctedDataSet = dataSetGroupMap.recomposeDataSet();
+//		DataSet correctedDataSet = dataSetGroupMap.recomposeDataSet();
 
 		String exportCorrectedFilename = ObjectUtility.getPackagePath(DataSet.class).replace("bin", "src") + "corrected.csv";
-		correctedDataSet.save(exportCorrectedFilename);
+		dataSet.save(exportCorrectedFilename);
 //		new FakeDialog(dataSet.getUI());
 		System.exit(0);
 	}
--- trunk/src/repicea/stats/data/DataSequence.java
+++ trunk/src/repicea/stats/data/DataSequence.java
@@ -22,5 +22,77 @@ import java.util.HashMap;
 import java.util.List;
 
 public class DataSequence extends HashMap<Object, List<Object>> {
+	
+	
+	public static class ActionOnPattern {
+		protected void doAction(DataPattern pattern, Object... parms) {}
+	}
+	
+	
+	protected static enum Mode {Total, Partial}
+	
+	private final boolean isEmptyPatternAccepted;
+	private final Mode mode;
+	private final ActionOnPattern action;
+	
+	public DataSequence(boolean isEmptyPatternAccepted, Mode mode, ActionOnPattern action) {
+		this.isEmptyPatternAccepted = isEmptyPatternAccepted;
+		this.mode = mode;
+		this.action = action;
+	}
 
+	
+	public boolean testPattern(DataPattern pattern, List<Object> exclusions) {
+		switch (mode) {
+		case Total:
+			boolean fit = doesFitInThisSequence(pattern, exclusions);
+			if (fit) {
+				action.doAction(pattern);
+			}
+			return fit;
+		case Partial:
+			Integer index = doesPartlyFitInThisSequence(pattern, exclusions);
+			if (index != null) {
+				action.doAction(pattern, index);
+			}
+			return  index != null;
+		}
+		return false;
+	}
+	
+	
+	private boolean doesFitInThisSequence(DataPattern pattern, List<Object> exclusions) {
+		DataPattern cleanPattern = pattern.getTrimmedPattern(exclusions);
+		if (cleanPattern == null || cleanPattern.isEmpty()) {
+			return isEmptyPatternAccepted;
+		} else  if (cleanPattern.size() == 1) {
+			Object singleObj = cleanPattern.get(0);
+			return containsKey(singleObj);
+		} else {
+			for (int i = 1; i < cleanPattern.size(); i++) {
+				Object obj0 = cleanPattern.get(i - 1);
+				Object obj1 = cleanPattern.get(i);
+				if (!containsKey(obj0) || !get(obj0).contains(obj1)) {
+					return false;
+				} 
+			}
+			return true;
+		}
+	}
+
+	private Integer doesPartlyFitInThisSequence(DataPattern pattern, List<Object> exclusions) {
+		if (pattern.size() > 2) {
+			for (int i = 1; i < pattern.size(); i++) {
+				Object obj0 = pattern.get(i - 1);
+				Object obj1 = pattern.get(i);
+				if (containsKey(obj0) && get(obj0).contains(obj1)) {
+					return i;
+				} 
+			}
+		} 
+		return null;
+	}
+
+	
+	
 }
