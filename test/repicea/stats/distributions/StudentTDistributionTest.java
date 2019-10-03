2bbbee8da625f81d8a82f2ecf4e2d9e7451c5d3d
donenrique <50317186+donenrique24@users.noreply.github.com>
2019-10-03 18:23:39 +0000

    Junit test for Student's distribution

--- test/repicea/stats/distributions/StudentTDistributionTest.java
+++ test/repicea/stats/distributions/StudentTDistributionTest.java
@@ -4,6 +4,7 @@ package repicea.stats.distributions;
 import org.junit.Assert;
 import org.junit.Test;
 
+import repicea.math.Matrix;
 import repicea.stats.estimates.ConfidenceInterval;
 import repicea.stats.estimates.MonteCarloEstimate;
 
@@ -72,5 +73,34 @@ public class StudentTDistributionTest {
 		Assert.assertEquals("Testing quantile 0.025", quantile025, -2.085963, 5E-2);
 		Assert.assertEquals("Testing quantile 0.975", quantile975, 2.085963, 5E-2);
 	}
+	
+
+	@Test
+	public void testRandomNumberGenerationWithNonCentered10DegreesOfFreedom() {
+		
+		MonteCarloEstimate estimate = new MonteCarloEstimate();
+		Matrix mean = new Matrix(1,1);
+		mean.m_afData[0][0] = 10;
+		Matrix variance = new Matrix(1,1);
+		variance.m_afData[0][0] = 20;
+		StudentTDistribution dist = new StudentTDistribution(mean, variance, 10);
+		for (int i = 0; i < 5000000; i++) {
+			estimate.addRealization(dist.getRandomRealization());
+		}
+		double actualMean = estimate.getMean().m_afData[0][0];
+		double actualVariance = estimate.getVariance().m_afData[0][0];
+		ConfidenceInterval ci = estimate.getConfidenceIntervalBounds(.95);
+		double quantile025 = ci.getLowerLimit().m_afData[0][0];
+		double quantile975 = ci.getUpperLimit().m_afData[0][0];
+		double expectedMean = dist.getMean().m_afData[0][0];
+		double expectedVariance = dist.getVariance().m_afData[0][0];
+		double expectedQuantile025 = -2.228139 * Math.sqrt(20) + 10;
+		double expectedQuantile975 = 2.228139 * Math.sqrt(20) + 10;
+		Assert.assertEquals("Testing the mean", expectedMean, actualMean, 2E-3);
+		Assert.assertEquals("Testing the variance", expectedVariance, actualVariance, 5E-2);
+		Assert.assertEquals("Testing quantile 0.025", quantile025, expectedQuantile025, 5E-2);
+		Assert.assertEquals("Testing quantile 0.975", quantile975, expectedQuantile975, 5E-2);
+	}
+
 
 }
