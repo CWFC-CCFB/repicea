152d520aa2c0cc5220991e26d57584102e03f207
donenrique <donenrique@14d3ad2c-f47e-48ea-b096-69a619d0e6a4>
2020-03-24 15:08:08 +0000

    Adding Junit test for Observation class

---
+++ trunk/test/repicea/stats/data/ObservationTests.java
@@ -0,0 +1,59 @@
+package repicea.stats.data;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ObservationTests {
+
+	
+	@Test
+	public void testEquality1() {
+		Object[] values1 = new Object[] {"Allo", 2.345332, 1};
+		Object[] values2 = new Object[] {"Allo", 2.345332, 1};
+		Observation obs1 = new Observation(values1);
+		Observation obs2 = new Observation(values2);
+		Assert.assertTrue("Testing that the two observations are equal",
+				obs1.isEqualToThisObservation(obs2));
+	}
+
+	@Test
+	public void testEquality2() {
+		Object[] values1 = new Object[] {"Allo", 2.345332, 1};
+		Observation obs1 = new Observation(values1);
+		Observation obs2 = null;
+		Assert.assertTrue("Testing that one observation compared to null returns false",
+				!obs1.isEqualToThisObservation(obs2));
+	}
+
+	@Test
+	public void testEquality3() {
+		Object[] values1 = new Object[] {"Allo", 2.345332, 1};
+		Object[] values2 = new Object[] {"Allo", 2.345332, 1, "allo"};
+		Observation obs1 = new Observation(values1);
+		Observation obs2 = new Observation(values2);
+		Assert.assertTrue("Testing that the two observations are unequal",
+				!obs1.isEqualToThisObservation(obs2));
+	}
+
+
+	@Test
+	public void testEqualityForDoubles() {
+		Object[] values1 = new Object[] {"Allo", 2.3453320000000000000000, 1};
+		Object[] values2 = new Object[] {"Allo", 2.3453320000000000000001, 1};
+		Observation obs1 = new Observation(values1);
+		Observation obs2 = new Observation(values2);
+		Assert.assertTrue("Testing that the two doubles are equal",
+				obs1.isEqualToThisObservation(obs2));
+	}
+
+	@Test
+	public void testInequalityForDoubles() {
+		Object[] values1 = new Object[] {"Allo", 2.34533200, 1};
+		Object[] values2 = new Object[] {"Allo", 2.34533202, 1};
+		Observation obs1 = new Observation(values1);
+		Observation obs2 = new Observation(values2);
+		Assert.assertTrue("Testing that the two doubles are unequal",
+				!obs1.isEqualToThisObservation(obs2));
+	}
+
+}
