52186eab03767ad3a7bd13bd9c47a068739c1a1a
donenrique <donenrique@14d3ad2c-f47e-48ea-b096-69a619d0e6a4>
2020-04-17 01:18:53 +0000

    Bug correction in the residual implementation in the height diameter relationships

--- trunk/test/repicea/net/server/ServerTests.java
+++ trunk/test/repicea/net/server/ServerTests.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Vector;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import repicea.lang.codetranslator.REnvironment;
