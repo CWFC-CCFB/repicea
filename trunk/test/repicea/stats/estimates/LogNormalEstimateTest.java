c2e8f09fa151bfac61e37a76694c09c996008685
donenrique <donenrique@14d3ad2c-f47e-48ea-b096-69a619d0e6a4>
2021-01-22 17:02:55 +0000

    LogNormalEstimate class now produces confidence intervals as well as random deviates

--- trunk/test/repicea/stats/estimates/LogNormalEstimateTest.java
+++ trunk/test/repicea/stats/estimates/LogNormalEstimateTest.java
@@ -20,6 +20,7 @@ package repicea.stats.estimates;
 
 import org.junit.Assert;
 import org.junit.Test;
+import repicea.stats.distributions.utility.GaussianUtility;
 
 public class LogNormalEstimateTest {
 
@@ -45,10 +46,33 @@ public class LogNormalEstimateTest {
 		double variance = est.getDistribution().getVariance().m_afData[0][0];
 		Assert.assertEquals("Testing the variance", 0.5, variance, 1E-8);
 	}
-	
-	
-	// TODO implement random realization generation
 
-	
-	
+	@Test
+	public void testingConfidenceIntervalBounds() {
+		LogNormalEstimate est = new LogNormalEstimate(2, .5);
+		ConfidenceInterval ci = est.getConfidenceIntervalBounds(0.95);
+		double actualLowerBound = ci.getLowerLimit().m_afData[0][0];
+		double expectedLowerBound = Math.exp(2 - GaussianUtility.getQuantile(0.975) * Math.sqrt(.5));
+		Assert.assertEquals("Testing the lower bound", expectedLowerBound, actualLowerBound, 1E-8);
+		double actualUpperBound = ci.getUpperLimit().m_afData[0][0];
+		double expectedUpperBound = Math.exp(2 + GaussianUtility.getQuantile(0.975) * Math.sqrt(.5));
+		Assert.assertEquals("Testing the upper bound", expectedUpperBound, actualUpperBound, 1E-8);
+	}
+
+
+	@Test
+	public void testingRandomDeviates() {
+		MonteCarloEstimate mce = new MonteCarloEstimate();
+		LogNormalEstimate est = new LogNormalEstimate(9.487735836358526, 58.39602780205479, false);
+		for (int i = 0; i < 2000000; i++) {
+			mce.addRealization(est.getRandomDeviate());
+		}
+		double actualMean = mce.getMean().m_afData[0][0];
+		double expectedMean = est.getMean().m_afData[0][0];
+		Assert.assertEquals("Testing mean", expectedMean, actualMean, 1E-2);
+		double actualVariance = mce.getVariance().m_afData[0][0];
+		double expectedVariance = est.getVariance().m_afData[0][0];
+		Assert.assertEquals("Testing variance", expectedVariance, actualVariance, 4E-1);
+	}
+
 }
