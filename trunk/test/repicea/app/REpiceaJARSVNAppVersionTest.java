feacb91f039a9e8637401af5910e4109823ecbc4
donenrique <donenrique@14d3ad2c-f47e-48ea-b096-69a619d0e6a4>
2021-02-07 20:26:12 +0000

    Fixing the version compiler

---
+++ trunk/test/repicea/app/REpiceaJARSVNAppVersionTest.java
@@ -0,0 +1,40 @@
+/*
+ * This file is part of the repicea-util library.
+ *
+ * Copyright (C) 2009-2021 Mathieu Fortin for Rouge Epicea.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed with the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * Please see the license at http://www.gnu.org/copyleft/lesser.html.
+ */
+package repicea.app;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import repicea.util.ObjectUtility;
+
+public class REpiceaJARSVNAppVersionTest {
+	
+	@Test
+	public void compileAndRetrieveRevision() {
+		if (!ObjectUtility.isEmbeddedInJar(REpiceaJARSVNAppVersionCompiler.class)) {
+			REpiceaJARSVNAppVersionCompiler.main(null);
+		}
+		String build = REpiceaJARSVNAppVersion.getInstance().getBuild();
+		try {
+			Integer.parseInt(build);
+		} catch (NumberFormatException e) {
+			Assert.fail("The revision cannot be parsed to an integer!");
+		}
+	}
+}
---
+++ trunk/test/repicea/util/ObjectUtilityTest.java
@@ -0,0 +1,38 @@
+/*
+ * This file is part of the repicea library.
+ *
+ * Copyright (C) 2009-2021 Mathieu Fortin for Rouge Epicea.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed with the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * Please see the license at http://www.gnu.org/copyleft/lesser.html.
+ */
+package repicea.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ObjectUtilityTest {
+
+	@Test
+	public void testingIfAssertClassInJar() {
+		boolean isInJar = ObjectUtility.isEmbeddedInJar(Assert.class);
+		Assert.assertTrue("Testing if the Assert class is in a jar.", isInJar);
+	}
+	
+	@Test
+	public void testingIfThisClassInJar() {
+		boolean isInJar = ObjectUtility.isEmbeddedInJar(ObjectUtilityTest.class);
+		Assert.assertTrue("Testing that this class is not in a jar.", !isInJar);
+	}
+
+}
