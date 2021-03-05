aacf6607d5cb6803e10f78c476aec72732f21523
donenrique <donenrique@14d3ad2c-f47e-48ea-b096-69a619d0e6a4>
2021-03-05 14:31:45 +0000

    Adding the toArray method to the Matrix class

--- trunk/test/repicea/math/MatrixTests.java
+++ trunk/test/repicea/math/MatrixTests.java
@@ -86,6 +86,46 @@ public class MatrixTests {
 		boolean equalToIdentity = !diff.anyElementLargerThan(1E-15);
 		Assert.assertEquals(true, equalToIdentity);
 	}
+
+	
+	/**
+	 * This test is performed on the calculation of the inverse of a blocked matrix.
+	 */
+	@Test
+	public void toArrayTest() {
+		
+		Matrix mat = new Matrix(9,9);
+		mat.setValueAt(0, 0, 5.49);
+		mat.setValueAt(0, 4, 1.85);
+		mat.setValueAt(1, 1, 3.90);
+		mat.setValueAt(2, 2, 2.90);
+		mat.setValueAt(2, 3, 1.02);
+		mat.setValueAt(2, 5, 0.70);
+		mat.setValueAt(2, 6, 0.76);
+		mat.setValueAt(2, 7, 0.77);
+		mat.setValueAt(2, 8, 0.80);
+		mat.setValueAt(3, 3, 3.20);
+		mat.setValueAt(3, 5, 0.89);
+		mat.setValueAt(3, 6, 0.87);
+		mat.setValueAt(3, 7, 0.89);
+		mat.setValueAt(3, 8, 0.93);
+		mat.setValueAt(4, 4, 4.55);
+		mat.setValueAt(5, 5, 2.70);
+		mat.setValueAt(5, 6, 0.66);
+		mat.setValueAt(5, 7, 0.67);
+		mat.setValueAt(5, 8, 0.70);
+		mat.setValueAt(6, 6, 2.69);
+		mat.setValueAt(6, 7, 0.66);
+		mat.setValueAt(6, 8, 0.69);
+		mat.setValueAt(7, 7, 2.70);
+		mat.setValueAt(7, 8, 0.70);
+		mat.setValueAt(8, 8, 2.76);
+		
+		double[][] array = mat.toArray();
+		Matrix mat2 = new Matrix(array);
+		boolean isThereAnyDiff = mat.subtract(mat2).getAbsoluteValue().anyElementLargerThan(1E-8);
+		Assert.assertTrue("Testing that there is no difference between the two matrices", !isThereAnyDiff);
+	}
 	
 	/**
 	 * This test is performed on the calculation of the inverse of a large symmetric matrix with many zero cells.
@@ -139,6 +179,7 @@ public class MatrixTests {
 		oMat.multiply(oMat);
 		System.out.println("Elapsed time = " + ((System.currentTimeMillis() - startingTime) * .001));
 	}
+	
 	public static void main(String[] args) throws IOException {
 		MatrixTests test = new MatrixTests();
 //		test.speedTestInversionMatrix(100);
