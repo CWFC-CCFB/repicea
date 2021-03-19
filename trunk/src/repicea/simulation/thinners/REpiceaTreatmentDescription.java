453e847dc9a81c47e1f0755c6d21c39920ad742e
donenrique <donenrique@14d3ad2c-f47e-48ea-b096-69a619d0e6a4>
2021-03-19 15:29:09 +0000

    Adding the REpiceaTreatmentDescription interface for a better handling of harvest occurrence

---
+++ trunk/src/repicea/simulation/thinners/REpiceaTreatmentDescription.java
@@ -0,0 +1,40 @@
+/*
+ * This file is part of the repicea library.
+ *
+ * Copyright (C) 2009-2021 Mathieu Fortin for Rouge-Epicea
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed with the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * Please see the license at http://www.gnu.org/copyleft/lesser.html.
+ */
+package repicea.simulation.thinners;
+
+/**
+ * The REpiceaTreatmentDescription interface ensures a particular description of treatment contains 
+ * a treatment type and a delay before re-entry.
+ * @author Mathieu Fortin - March 2021
+ */
+public interface REpiceaTreatmentDescription {
+
+	/**
+	 * Provide the treatment type associated with this instance of REpiceaTreatmentDescription.
+	 * @return an Enum that stands for the treatment type
+	 */
+	public Enum getTreatmentType();
+	
+	/**
+	 * Provide the number of years before re-entry. 
+	 * @return an integer that is the number of years.
+	 */
+	public int getDelayBeforeReentryYrs();
+	
+}
