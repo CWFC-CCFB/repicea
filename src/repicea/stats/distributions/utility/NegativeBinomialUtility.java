4816aeeb688b3abf520bd39bf31f43257d29b0a4
donenrique24 <mathieu.fortin.re@gmail.com>
2022-11-02 11:13:39 -0400

    Adding a quantile method in NegativeBinomialUtility

--- src/repicea/stats/distributions/utility/NegativeBinomialUtility.java
+++ src/repicea/stats/distributions/utility/NegativeBinomialUtility.java
@@ -82,4 +82,19 @@ public class NegativeBinomialUtility {
 		}
 	}
 
+	/**
+	 * Provide a quantile of the distribution.
+	 * @param the cumulative mass
+	 * @return a quantile
+	 */
+	public static int getQuantile(double cdfValue, double mean, double dispersion) {
+		if (cdfValue < 0 || cdfValue > 1) 
+			throw new InvalidParameterException("The cdfValue parameter should be a double between 0 and 1!");
+		double cumulativeMass = 0d;
+		int y = 0;
+		while(cumulativeMass < cdfValue) 
+			cumulativeMass += getMassProbability(y++, mean, dispersion); 
+		return --y;
+	}
+
 }
