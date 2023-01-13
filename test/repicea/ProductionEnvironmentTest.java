9e706e42ae11ee4347e1a6072027b8385ed89d91
donenrique24 <mathieu.fortin.re@gmail.com>
2023-01-13 14:45:43 -0500

    Migrating metamodel package to a distinct project

--- test/repicea/ProductionEnvironmentTest.java
+++ test/repicea/ProductionEnvironmentTest.java
@@ -3,17 +3,17 @@ package repicea;
 import org.junit.Assert;
 import org.junit.Test;
 
-import repicea.simulation.metamodel.MetaModelManager;
+import repicea.simulation.UseModeProvider;
 
 public class ProductionEnvironmentTest {
-	@Test 
 	
 	/**
 	 * This test will fail when used with a local class folder, but will succeed when running on a JAR file.
 	 * This test is excluded from the normal local tests and executed only with the integration tests from the JAR file
 	 */
+	@Test 
 	public void makeSurePackageIsRunningFromJAR() {
-		String resourceURL = MetaModelManager.class.getResource("MetaModelManager.class").toString();
+		String resourceURL = UseModeProvider.class.getResource("UseModeProvider.class").toString();
 		Assert.assertTrue(resourceURL.startsWith("jar:"));
 	}
 }
