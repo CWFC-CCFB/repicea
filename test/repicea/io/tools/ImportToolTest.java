61adeb7a7bc4263233acfb9bf10c39e6de1809ff
Mathieu Fortin <mathieu.fortin.re@gmail.com>
2025-09-24 20:58:48 -0400

    Removing legacy test on Access database

--- test/repicea/io/tools/ImportToolTest.java
+++ test/repicea/io/tools/ImportToolTest.java
@@ -25,34 +25,34 @@ public class ImportToolTest {
 		SerializerChangeMonitor.registerClassNameChange("repicea.test.tools.TestRecordReader$FieldID", "repicea.io.tools.RecordReaderImpl$FieldID");
 	}
 
-	@SuppressWarnings("unchecked")
-	@Test
-	public void testStrataFileReading() throws Exception {
-		String sourcePath = ObjectUtility.getPackagePath(ImportTest.class) + "TEST6152.accdb";
-		
-		String targetPath = REpiceaSystem.getJavaIOTmpDir() + "TEST6152.accdb";
-		String testIfe = ObjectUtility.getPackagePath(RecordReaderImpl.class) + "test.ife";
-		String results = ObjectUtility.getPackagePath(RecordReaderImpl.class) + "result1.xml";
-				
-		if (!FileUtility.copy(sourcePath, targetPath)) {
-			throw new IOException("Unable to copy the database file to tmp directory!");
-		}
-
-		RecordReaderImpl recordReader = new RecordReaderImpl();
-		ImportFieldManager ifm = ImportFieldManager.createImportFieldManager(recordReader, testIfe, targetPath, "TEST6152");
-		recordReader.initInScriptMode(ifm);
-		List<String> strataList = recordReader.getGroupList();
-//		XmlSerializer serializer = new XmlSerializer(results);
-//		serializer.writeObject(strataList);
-		XmlDeserializer deserializer = new XmlDeserializer(results);
-		List<String>  refStrataList = (List<String>) deserializer.readObject();
-		assertEquals("Testing for number of elements", refStrataList.size(), strataList.size());
-		for (int i = 0; i < strataList.size(); i++) {
-			assertEquals("Testing for group name at index " + ((Integer) i).toString(), 
-					refStrataList.get(i),
-					strataList.get(i));
-		}
-	}
+//	@SuppressWarnings("unchecked")
+//	@Test
+//	public void testStrataFileReading() throws Exception {
+//		String sourcePath = ObjectUtility.getPackagePath(ImportTest.class) + "TEST6152.accdb";
+//		
+//		String targetPath = REpiceaSystem.getJavaIOTmpDir() + "TEST6152.accdb";
+//		String testIfe = ObjectUtility.getPackagePath(RecordReaderImpl.class) + "test.ife";
+//		String results = ObjectUtility.getPackagePath(RecordReaderImpl.class) + "result1.xml";
+//				
+//		if (!FileUtility.copy(sourcePath, targetPath)) {
+//			throw new IOException("Unable to copy the database file to tmp directory!");
+//		}
+//
+//		RecordReaderImpl recordReader = new RecordReaderImpl();
+//		ImportFieldManager ifm = ImportFieldManager.createImportFieldManager(recordReader, testIfe, targetPath, "TEST6152");
+//		recordReader.initInScriptMode(ifm);
+//		List<String> strataList = recordReader.getGroupList();
+////		XmlSerializer serializer = new XmlSerializer(results);
+////		serializer.writeObject(strataList);
+//		XmlDeserializer deserializer = new XmlDeserializer(results);
+//		List<String>  refStrataList = (List<String>) deserializer.readObject();
+//		assertEquals("Testing for number of elements", refStrataList.size(), strataList.size());
+//		for (int i = 0; i < strataList.size(); i++) {
+//			assertEquals("Testing for group name at index " + ((Integer) i).toString(), 
+//					refStrataList.get(i),
+//					strataList.get(i));
+//		}
+//	}
 
 	
 	/*
